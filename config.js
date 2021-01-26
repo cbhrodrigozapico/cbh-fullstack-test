c84cbdd651a63eeccd8f693458cecbcb6eb398fa
Rodrigo Zapico <rodrigo.zapico@clipboardhealth.com>
2021-01-25 21:22:10 -0300

    fixing prod env

--- config.js
+++ config.js
@@ -1,12 +1,5 @@
-
-const devConfig = {
-	apiurl: 'http://localhost:3000'
+const config = {
+	apiurl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
 }
-
-const prodConfig = {
-	apiurl: 'https://cbh-fullstack-test.vercel.app/'
-}
-
-const config = process.env.ENV === 'PROD' ? prodConfig : devConfig
 
 export default config
